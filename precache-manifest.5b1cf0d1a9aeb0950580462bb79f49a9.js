5395df568a02da8d47c588b844367b02e9079dee
Tengosan <HM.Lost.inc@yandex.ru@domain.com>
2020-02-24 12:41:46 +0700

    Updates

---
+++ precache-manifest.5b1cf0d1a9aeb0950580462bb79f49a9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea84278a796e99a079e1c9aea976c4a6",
+    "url": "/Booking_Hotel/index.html"
+  },
+  {
+    "revision": "9006254e33ff63fea06f",
+    "url": "/Booking_Hotel/static/css/2.4d44a405.chunk.css"
+  },
+  {
+    "revision": "c2b67c630e8d201b3791",
+    "url": "/Booking_Hotel/static/css/main.65424139.chunk.css"
+  },
+  {
+    "revision": "9006254e33ff63fea06f",
+    "url": "/Booking_Hotel/static/js/2.1e7f0c9d.chunk.js"
+  },
+  {
+    "revision": "c2b67c630e8d201b3791",
+    "url": "/Booking_Hotel/static/js/main.c0af24ec.chunk.js"
+  },
+  {
+    "revision": "d860a3c5e18f6cd7855e",
+    "url": "/Booking_Hotel/static/js/runtime~main.13113406.js"
+  },
+  {
+    "revision": "bfd2c3c7908b00e7c3ab28663cc2384f",
+    "url": "/Booking_Hotel/static/media/Logo.bfd2c3c7.svg"
+  },
+  {
+    "revision": "d4e0b4973be52cb4ce6bf57a6d118d1f",
+    "url": "/Booking_Hotel/static/media/TOXIN.d4e0b497.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Booking_Hotel/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Booking_Hotel/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Booking_Hotel/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Booking_Hotel/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Booking_Hotel/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "59855e51f65ac7a4589710f27ff68b30",
+    "url": "/Booking_Hotel/static/media/image1.59855e51.png"
+  },
+  {
+    "revision": "3ced6ab86daa7f986fff322b731c68ba",
+    "url": "/Booking_Hotel/static/media/image2.3ced6ab8.png"
+  },
+  {
+    "revision": "6364319ee42285033f1b19f9d226ff59",
+    "url": "/Booking_Hotel/static/media/image3.6364319e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Match_Grid/precache-manifest.f042a2a2a65daf842d781c529d1d7fcc.js"
+  "/Booking_Hotel/precache-manifest.5b1cf0d1a9aeb0950580462bb79f49a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Match_Grid/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Booking_Hotel/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
