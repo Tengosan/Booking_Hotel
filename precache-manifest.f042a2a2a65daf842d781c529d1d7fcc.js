411c2d388f0b88f4a8e167c2596e8e5209903eb5
Tengosan <HM.Lost.inc@yandex.ru@domain.com>
2020-02-24 12:39:55 +0700

    Updates

---
+++ precache-manifest.f042a2a2a65daf842d781c529d1d7fcc.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60d71279f67e0cdf88b3f4f70d5daa6",
+    "url": "/Match_Grid/index.html"
+  },
+  {
+    "revision": "beb8e30690fab10f1607",
+    "url": "/Match_Grid/static/css/2.89239848.chunk.css"
+  },
+  {
+    "revision": "2f93735ed1ace2474c9e",
+    "url": "/Match_Grid/static/css/main.65424139.chunk.css"
+  },
+  {
+    "revision": "beb8e30690fab10f1607",
+    "url": "/Match_Grid/static/js/2.1e7f0c9d.chunk.js"
+  },
+  {
+    "revision": "2f93735ed1ace2474c9e",
+    "url": "/Match_Grid/static/js/main.1bc87349.chunk.js"
+  },
+  {
+    "revision": "aae841c7c709581a4f06",
+    "url": "/Match_Grid/static/js/runtime~main.801d7045.js"
+  },
+  {
+    "revision": "bfd2c3c7908b00e7c3ab28663cc2384f",
+    "url": "/Match_Grid/static/media/Logo.bfd2c3c7.svg"
+  },
+  {
+    "revision": "d4e0b4973be52cb4ce6bf57a6d118d1f",
+    "url": "/Match_Grid/static/media/TOXIN.d4e0b497.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Match_Grid/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Match_Grid/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Match_Grid/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Match_Grid/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Match_Grid/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "59855e51f65ac7a4589710f27ff68b30",
+    "url": "/Match_Grid/static/media/image1.59855e51.png"
+  },
+  {
+    "revision": "3ced6ab86daa7f986fff322b731c68ba",
+    "url": "/Match_Grid/static/media/image2.3ced6ab8.png"
+  },
+  {
+    "revision": "6364319ee42285033f1b19f9d226ff59",
+    "url": "/Match_Grid/static/media/image3.6364319e.png"
+  }
+]);
